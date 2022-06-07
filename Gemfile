bd6d043a3a719f5dffd184a9a19dcb739eb87cfa
Varsha Kulkarni <varsha@triveous.com>
2022-06-08 03:36:30 +0530

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+gem "jekyll-theme-so-simple"
+gem 'github-pages', group: :jekyll_plugins
+gem 'jemoji'
+gem 'jekyll-gist'
+group :jekyll_plugins do
+  gem "jekyll-paginate"
+end
---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome!",
+        "excerpt":"You will find here some debugging, some tips and tricks from my experience. ","categories": ["welcome"],
+        "tags": [],
+        "url": "http://localhost:4001/welcome/2022/05/03/welcome.html"
+      }]
